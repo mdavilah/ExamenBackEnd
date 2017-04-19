741cf501fc94c47e1c834007a359790f6d969bbb
Mario <mdavila0482@gmail.com>
2017-04-18 22:16:10 -0500

    octavo commit

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -3,4 +3,11 @@ class Inventory < ApplicationRecord
 	validates :NumSerie, presence: true
 	validates :size, numericality: {greater_than: 0}
 
+	after_destroy :destroy_works
+
+	private
+	def destroy_works
+		works.destroy_all
+	end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-	has_many :works
+	has_many :works, inverse_of: :user, dependent: :delete_all
 
 	validates :name, presence: true
+
+	after_destroy :destroy_works
+
+	private
+
+	def destroy_works
+		works.destroy_all
+	end
+
 end
